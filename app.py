9de4b7571e413934ca5c8ac26f83c6a66a7678e0
Ckorine <Keli.Dara-Ahato@viadee.de>
2020-06-18 22:38:34 +0200

    relocate base and home in folder templates and added base as start page

--- app.py
+++ app.py
@@ -1,5 +1,9 @@
-from flask import Flask
+from flask import Flask, render_template, request,session
 app = Flask(__name__)
+
+
+
+
 @app.route('/')
-def hello_world():
- return "Hello World"
+def index():
+ return render_template('base.html')
